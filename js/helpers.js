eb3a034f50b7a1ed51f2785add98504d5c5330b6
Numan BLK <numanblk@iMac.lan>
2026-02-28 14:32:08 +0100

    js-mapapp

---
+++ js/helpers.js
@@ -0,0 +1,54 @@
+import { defaultIcon, visitIcon, homeIcon, jobIcon, parkIcon } from "./ui.js";
+
+const VISIT = "visit"
+const HOME = "home"
+const JOB = "job"
+const PARK = "park"
+
+const ZIYARET = "Ziyaret"
+const EV = "ev"
+const IS = "Is"
+const PARK_YERI = "Park Yeri"
+const TANIMSIN = "Tanimsiz"
+
+
+
+//tarih formati ayarla
+export const formatDate = (date) => {
+    return new Date(date).toLocaleDateString('tr',{
+        day: "2-digit",
+        month: "long",
+        year: "numeric"
+    })
+}
+
+//ikon alma durumu
+export const getNoteIcon = (status) => {
+    switch (status) {
+        case VISIT:
+                return visitIcon;
+        case HOME:
+                return homeIcon;
+        case JOB:
+                return jobIcon;
+        case PARK:
+                return parkIcon;
+        default:
+            return defaultIcon;
+    }
+}
+//status durumu
+export const getStatus = (status) => {
+    switch (status) {
+        case VISIT:
+                return ZIYARET;
+        case HOME:
+                return EV;
+        case JOB:
+                return IS;
+        case PARK:
+                return PARK_YERI;
+        default:
+            return TANIMSIN;
+    }
+}
